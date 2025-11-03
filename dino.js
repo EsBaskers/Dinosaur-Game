67615ab97b01f244fb2c45373315270d4ef1af4b
Skolnieks <skolnieks@vtdt.edu.lv>
2025-11-03 13:13:45 +0200

    Added js files

--- dino.js
+++ dino.js
@@ -19,3 +19,35 @@ let dino = {
     width : dinoWidth,
     height : dinoHeight
 }
+
+//cactus
+
+let cactusArray = [];
+
+let cactus1Width = 34;
+let cactus2Width = 69;
+let cactus3Width = 102;
+
+let cactusHeight = 70;
+let cactusX = 700;
+let cactusY = boardHeight - cactusHeight;
+
+let cactus1Img;
+let cactus2Img;
+let cactus3Img;
+
+//physics
+
+let velocityX = -8; 
+let velocityY = 0;
+let gravity = .4;
+
+let gameOver = false;
+let score = 0;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+
+    context = board.getContext("2d"); }
