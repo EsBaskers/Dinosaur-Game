c1f2ef2507513b72468abc36f43cd5229dcd3746
Skolnieks <skolnieks@vtdt.edu.lv>
2025-11-03 13:01:27 +0200

    Updated files

---
+++ dino.js
@@ -0,0 +1,21 @@
+//board stuff
+
+let board;
+let boardWidth = 1500;
+let boardHeight = 500;
+let context;
+
+//dino
+
+let dinoWidth = 90;
+let dinoHeight = 95;
+let dinoX = 50;
+let dinoY = boardHeight - dinoHeight;
+let dinoImg;
+
+let dino = {
+    x : dinoX,
+    y : dinoY,
+    width : dinoWidth,
+    height : dinoHeight
+}
